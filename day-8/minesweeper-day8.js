acfcb8432b1a7e4fa54fb165b27af68f04678b12
Sam Yang <super8989@gmail.com>
2018-05-23 23:05:45 -0400

    minesweeper day8

---
+++ day-8/minesweeper-day8.js
@@ -0,0 +1,19 @@
+const exampleBoard = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+];
+
+const printBoard = board => {
+  console.log('Current Board: ');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+printBoard(exampleBoard);
+
+exampleBoard[0][1] = '1';
+exampleBoard[2][2] = 'B';
+
+printBoard(exampleBoard);
