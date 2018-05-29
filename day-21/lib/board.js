d18ca5c39ad1e4496076ff0714671d6092767bae
Sam Yang <super8989@gmail.com>
2018-05-28 22:49:06 -0400

    board.js

---
+++ day-21/lib/board.js
@@ -0,0 +1,159 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+//export the board class
+
+//When a user creates an instance of a board (using the constructor), they will need to specify the size
+//of the board as well as the number of bombs on the board (which is what the parameters represent).
+var Board = exports.Board = function () {
+  function Board(numberOfRows, numberOfColumns, numberOfBombs) {
+    _classCallCheck(this, Board);
+
+    this._numberOfBombs = numberOfBombs;
+    this._numberOfTiles = numberOfRows * numberOfColumns;
+    this._playerBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
+    this._bombBoard = Board.generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
+  }
+
+  //create a getter method for playerBoard
+
+
+  _createClass(Board, [{
+    key: 'flipTile',
+
+
+    //add the flipTile() function to the Board class.
+
+    // check if tile is already flipped, if so, return
+    // check if tile is bomb, if so, place bomb on player Board
+    // if tile is not a bomb, place number of surrounding bombs on player board
+
+    value: function flipTile(rowIndex, columnIndex) {
+      if (this._playerBoard[rowIndex][columnIndex] !== ' ') {
+        console.log('This tile has already been flipped!');
+        return;
+      } else if (this._bombBoard[rowIndex][columnIndex] === 'B') {
+        this._playerBoard[rowIndex][columnIndex] = 'B';
+      } else {
+        this._playerBoard[rowIndex][columnIndex] = this.getNumberOfNeighborBombs(rowIndex, columnIndex);
+      }
+      this._numberOfTiles--;
+    }
+
+    //add the getNumberOfNeighborBombs() function to the Board class.
+
+  }, {
+    key: 'getNumberOfNeighborBombs',
+    value: function getNumberOfNeighborBombs(rowIndex, columnIndex) {
+      var _this = this;
+
+      var neighborOffsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
+      var numberOfRows = this._bombBoard.length;
+      var numberOfColumns = this._bombBoard[0].length;
+
+      var numberOfBombs = 0;
+
+      neighborOffsets.forEach(function (offset) {
+        var neighborRowIndex = rowIndex + offset[0];
+        var neighborColumnIndex = columnIndex + offset[1];
+        // check to see if row and column are valid tile values on the board
+        if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns) {
+          if (_this._bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+            numberOfBombs++;
+          }
+        }
+      });
+      return numberOfBombs;
+    }
+  }, {
+    key: 'hasSafeTiles',
+
+
+    //add a method called hasSafeTiles - will need to check the numberOfTileson the board versus the numberOfBombs on the board.
+    value: function hasSafeTiles() {
+      return this._numberOfTiles !== this._numberOfBombs;
+    }
+  }, {
+    key: 'print',
+    value: function print() {
+      console.log(this._playerBoard.map(function (row) {
+        return row.join(' | ');
+      }).join('\n'));
+    }
+
+    //  const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    //    let board = [];
+    //       for loop iterating through numberOfRows
+    //         Create an empty row array
+    //         for loop iterating through numberOfColumns
+    //           Push the empty spaces onto the row array
+    //         Push the row onto the board array
+    //       Return the board array
+    //  };
+
+  }, {
+    key: 'playerBoard',
+    get: function get() {
+      return this._playerBoard;
+    }
+  }], [{
+    key: 'generatePlayerBoard',
+    value: function generatePlayerBoard(numberOfRows, numberOfColumns) {
+      var board = [];
+
+      for (var rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        var row = [];
+        for (var columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+          row.push(' ');
+        }
+        board.push(row);
+      }
+      return board;
+    }
+  }, {
+    key: 'generateBombBoard',
+
+
+    //  while (numberOfBombsPlaced < numberOfBombs) {
+    //     Generate a random row index
+    //     Generate a random column index
+    //     if the board[randomRowIndex][randomColumnIndex] does not have a bomb
+    //     Place the bomb at that row and columns
+    //     Increment numberOfBombsPlaced
+    //   };
+    value: function generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs) {
+      var board = [];
+
+      for (var rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        var row = [];
+        for (var columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+          row.push(null);
+        }
+        board.push(row);
+      }
+
+      var numberOfBombsPlaced = 0;
+
+      while (numberOfBombsPlaced < numberOfBombs) {
+        // This code has the potential to place bombs on top of bombs, this will be fixed with control flow.
+        var randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        var randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+          board[randomRowIndex][randomColumnIndex] = 'B';
+          numberOfBombsPlaced++;
+        }
+      }
+      return board;
+    }
+  }]);
+
+  return Board;
+}();
